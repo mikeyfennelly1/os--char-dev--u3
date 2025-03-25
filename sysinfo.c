4c935da036dc8bfc6c3f113baab49b67ea157eae
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-25 15:38:22 +0000

    successful reading for all info types

--- sysinfo.c
+++ sysinfo.c
@@ -12,13 +12,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/ioctl.h>
 
 #define SYSINFO_DEVICE "/dev/sysinfo"
 #define READ_BUF_SIZE 256
+#define SET_CIT_CPU _IOW('C',CPU, int)          // set the current_info_type to cpu
+#define SET_CIT_MEM _IOW('M', MEMORY, int)      // set the current_info_type to memory
+#define SET_CIT_DISK _IOW('D', DISK, int)       // // set the current_info_type to disk
 
-int change_current_info_type(int mode);
+int change_current_info_type(int dev_fd, int mode);
 char* get_sysinfo(int info_type);
-char* read_device(void);
+char* read_device(int fd);
 
 /**
  * Get sysinfo based on 1 of 3 sysinfo types.
@@ -37,21 +41,33 @@ char* get_sysinfo(int info_type)
     {
         return NULL;
     }
+
+    // open the /dev node
+    int fd = open(SYSINFO_DEVICE, O_RDONLY);
+    if (fd < 0)
+    {
+        perror("Could not open /dev node\n");
+        exit(EXIT_FAILURE);
+    }
     
     // change the current_info_type of the 
     // device to the requested info_type
-    int ret = change_current_info_type(info_type);
+    int ret = change_current_info_type(fd, info_type);
     if (ret != 0)
     {
         return NULL;
     }
 
     // read the device
-    char* info = read_device();
-    if (info == NULL)
+    char* sysinfo = read_device(fd);
+    if (sysinfo == NULL)
     {
         return NULL;
     }
+
+    close(fd);
+
+    return sysinfo;
 }
 
 /**
@@ -59,8 +75,33 @@ char* get_sysinfo(int info_type)
  * 
  * @return status code, indicating success of ioctl operation.
  */
-int change_current_info_type(int mode)
+int change_current_info_type(int dev_fd, int mode)
 {
+    if (!(mode >= 1 && mode <=3))
+    {
+        printf("ERROR: No corresponding device info mode for integer: %d\n", mode);
+        return EXIT_FAILURE;
+    }
+
+    if (mode == CPU)
+    {
+        if (ioctl(dev_fd, SET_CIT_CPU, CPU) == -1) {
+            perror("ioctl SET_CIT_CPU failed");
+        }
+    }
+    if (mode == MEMORY)
+    {
+        if (ioctl(dev_fd, SET_CIT_MEM, MEMORY) == -1) {
+            perror("ioctl SET_CIT_MEM failed");
+        }
+    }
+    if (mode == DISK)
+    {
+        if (ioctl(dev_fd, SET_CIT_DISK, DISK) == -1) {
+            perror("ioctl SET_CIT_DISK failed");
+        }
+    }
+
     return 0;
 }
 
@@ -70,22 +111,14 @@ int change_current_info_type(int mode)
  * @return - the contents of the read if successful
  *              else NULL.
  */
-char* read_device(void)
+char* read_device(int fd)
 {
     char* sysinfo = malloc(READ_BUF_SIZE);
-
-    // open the device
-    int dev_fd = open(SYSINFO_DEVICE, O_RDONLY);
-    if (dev_fd < 0)
-    {
-        printf("Could not read %s\n", SYSINFO_DEVICE);
-        return NULL;
-    }
 
     // read the file until EOF
     int ret;
     ssize_t bytes_read;
-    while((bytes_read = read(dev_fd, sysinfo, READ_BUF_SIZE - 1)) > 0)
+    while((bytes_read = read(fd, sysinfo, READ_BUF_SIZE - 1)) > 0)
     {
         sysinfo[bytes_read] = '\0';
         printf("%s", sysinfo);
@@ -97,24 +130,19 @@ char* read_device(void)
         return NULL;
     }
 
-    ret = close(dev_fd);
-    if (ret < 0)
-    {
-        printf("Could not close the device: %s\n", SYSINFO_DEVICE);
-        exit(EXIT_FAILURE);
-    }
-
     return sysinfo;
 }
 
 int main(void)
 {
-    char* contents = read_device();
-    if (contents == NULL)
-    {
-        printf("Contents are null\n");
-        exit(EXIT_FAILURE);
-    }
+    char* cpu_contents = get_sysinfo(CPU);
+    free(cpu_contents);
+
+    char* mem_contents = get_sysinfo(MEMORY);    
+    free(mem_contents);
+
+    char* disk_contents = get_sysinfo(DISK);
+    free(disk_contents);
 
     return EXIT_SUCCESS;
 }
