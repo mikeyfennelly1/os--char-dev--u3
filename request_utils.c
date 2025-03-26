26cf9c0aefdfad3099292e905818f93d53000822
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-26 11:18:54 +0000

    server working for all endpoints

--- request_utils.c
+++ request_utils.c
@@ -64,61 +64,59 @@ parse_request(char *request_buffer,
  * @param path - the client's requested route.
  * 
  */
-void
-handle_request(int client_socket,
-               const char *method,
-               const char *path)
-{
+void handle_request(int client_socket, const char *method, const char *path) {
     char response[BUFFER_SIZE];
-
+    char sysinfo[BUFFER_SIZE];
+    int content_length;
+    
     printf("path: %s\n", path);
     printf("method: %s\n", method);
 
     pthread_mutex_lock(&mutex_get_sysinfo);
-    if (strcmp(method, "GET") == 0 && strcmp(path, "/cpu") == 0) {
-        sprintf(response, 
+    if (strcmp(method, "GET") == 0) {
+        if (strcmp(path, "/cpu") == 0) {
+            snprintf(sysinfo, BUFFER_SIZE, "%s", get_sysinfo(CPU));
+        } else if (strcmp(path, "/memory") == 0) {
+            snprintf(sysinfo, BUFFER_SIZE, "%s", get_sysinfo(MEMORY));
+        } else if (strcmp(path, "/disk") == 0) {
+            snprintf(sysinfo, BUFFER_SIZE, "%s", get_sysinfo(DISK));
+        } else {
+            pthread_mutex_unlock(&mutex_get_sysinfo);
+            snprintf(response, BUFFER_SIZE, 
+                "HTTP/1.1 404 Not Found\r\n"
+                "Content-Length: 13\r\n"
+                "Content-Type: text/plain\r\n"
+                "\r\n"
+                "404 Not Found");
+            send(client_socket, response, strlen(response), 0);
+            close(client_socket);
+            return;
+        }
+
+        pthread_mutex_unlock(&mutex_get_sysinfo);
+        
+        content_length = strlen(sysinfo);
+        int header_size = snprintf(response, BUFFER_SIZE, 
             "HTTP/1.1 200 OK\r\n"
             "Content-Type: application/json\r\n"
-            "Content-Length: 20\r\n"
-            "\r\n"
-            "{\"data\": \"cpu\"}");
+            "Content-Length: %d\r\n"
+            "\r\n",
+            content_length);
 
-            char* sysinfo = get_sysinfo(CPU);
-            printf("%s\n", sysinfo);
-
-    } else if (strcmp(method, "GET") == 0 && strcmp(path, "/memory") == 0) {
-        sprintf(response, 
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: application/json\r\n"
-            "Content-Length: 20\r\n"
-            "\r\n"
-            "{\"data\": \"memory\"}");
-
-            char* sysinfo = get_sysinfo(MEMORY);
-            printf("%s\n", sysinfo);
-
-    } else if (strcmp(method, "GET") == 0 && strcmp(path, "/disk") == 0) {
-        sprintf(response, 
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: application/json\r\n"
-            "Content-Length: 20\r\n"
-            "\r\n"
-            "{\"data\": \"disk\"}");
-
-            char* sysinfo = get_sysinfo(DISK);
-            printf("%s\n", sysinfo);
-
+        if (header_size < BUFFER_SIZE) {
+            strncat(response, sysinfo, BUFFER_SIZE - header_size - 1);
+        }
     } else {
-        sprintf(response, 
-            "HTTP/1.1 404 Not Found\r\n"
-            "Content-Length: 13\r\n"
+        snprintf(response, BUFFER_SIZE,
+            "HTTP/1.1 405 Method Not Allowed\r\n"
+            "Content-Length: 23\r\n"
             "Content-Type: text/plain\r\n"
             "\r\n"
-            "404 Not Found");
+            "405 Method Not Allowed");
+        pthread_mutex_unlock(&mutex_get_sysinfo);
     }
-    pthread_mutex_unlock(&mutex_get_sysinfo);
-    printf("Response:\n\n %s\n", response);
 
+    printf("Response:\n\n %s\n", response);
     send(client_socket, response, strlen(response), 0);
     close(client_socket);
 }
